cd97fa33da1d96f7327ed81aa6ee6018ed16e2ad
D-eepanshu <113091550+D-eepanshu@users.noreply.github.com>
2025-09-17 00:32:53 +0530

    Add solution for Replace Non-Coprime Numbers in Array

--- LeetCode/Replace-Non-Coprime-Numbers-in-Array/Solution.cpp
+++ LeetCode/Replace-Non-Coprime-Numbers-in-Array/Solution.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    // Function to calculate GCD
-    long long gcd(long long a, long long b) {
-        while (b != 0) {
-            long long temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+    int lcm(int a, int b) {
+        return (1LL * a / __gcd(a, b)) * b;
     }
 
-    // Function to calculate LCM
-    long long lcm(long long a, long long b) {
-        return (a / gcd(a, b)) * b;  // avoid overflow
-    }
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<long long> st; // use long long to avoid overflow
-        for (int num : nums) {
-            long long x = num;
-            // Merge with stack top if non-coprime
-            while (!st.empty() && gcd(st.back(), x) > 1) {
-                x = lcm(st.back(), x);
-                st.pop_back();
-            }
-            st.push_back(x);
-        }
+        vector<int> v;
 
-        // Convert back to int
-        vector<int> ans;
-        for (auto v : st) ans.push_back((int)v);
-        return ans;
+        for (int x : nums) {
+            v.push_back(x);
+
+            while (v.size() > 1) {
+                int gc = __gcd(v[v.size()-1], v[v.size()-2]);
+                if (gc > 1) {
+                    int lc = lcm(v[v.size()-1], v[v.size()-2]);
+                    v.pop_back();
+                    v.pop_back();
+                    v.push_back(lc);
+                } else {
+                    break;
+                }
+            }
+        }
+        return v;
     }
 };
