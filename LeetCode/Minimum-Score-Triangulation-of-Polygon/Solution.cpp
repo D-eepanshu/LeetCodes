1db5f448716deb2a0813136d4e9457bc3e74a2e5
D-eepanshu <113091550+D-eepanshu@users.noreply.github.com>
2025-09-29 22:39:26 +0530

    Add solution for Minimum Score Triangulation of Polygon

---
+++ LeetCode/Minimum-Score-Triangulation-of-Polygon/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // dp[i][j] is the minimum score to triangulate polygon between i and j
+
+        for (int len = 3; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; ++k) {
+                    int cost = values[i] * values[k] * values[j];
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + cost);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};
